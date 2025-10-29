cce4576f680f7c8ad0f80ee0d2e1ab58f9fc6a3a
Shadan Ali <154227380+Shadan0786@users.noreply.github.com>
2025-10-29 12:06:40 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,42 @@
+// 🍔 Toggle Menu
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  navLinks.classList.toggle('active');
+});
+
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+  window.location.href = "project1.html"
+
+});
+
+const button2 = document.getElementById('btn2');
+button2.addEventListener('click', () => {
+  window.location.href = "project2.html"
+
+});
+
+const form = document.querySelector(".contact-form");
+const submitbutton = form.querySelector(".submitbtn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+ 
+  submitbutton.textContent = "Message Sent ";
+
+  
+  submitbutton.disabled = true;
+
+  
+  setTimeout(() => {
+    submitbutton.textContent = "Send Message";
+    submitbutton.disabled = false;
+  }, 2000);
+
+  // Clear form fields
+  form.reset();
+});
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// static files serve karne ke liye
+app.use(express.static(path.join(__dirname, 'public')));
+
+// default route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// server run karo
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
