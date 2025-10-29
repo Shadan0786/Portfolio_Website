7583c59f00082fd23d48e012f4031bfc07cbac4a
Shadan Ali <154227380+Shadan0786@users.noreply.github.com>
2025-10-29 12:02:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,36 @@ menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+  window.location.href = "project1.html"
 
+});
+
+const button2 = document.getElementById('btn2');
+button2.addEventListener('click', () => {
+  window.location.href = "project2.html"
+
+});
+
+const form = document.querySelector(".contact-form");
+const submitbutton = form.querySelector(".submitbtn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(); 
+
+ 
+  submitbutton.textContent = "Message Sent ";
+
+  
+  submitbutton.disabled = true;
+
+  
+  setTimeout(() => {
+    submitbutton.textContent = "Send Message";
+    submitbutton.disabled = false;
+  }, 2000);
+
+ 
+  form.reset();
+});
