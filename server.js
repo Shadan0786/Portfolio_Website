e820fb73fbb49ee1d01abba8173a4e8b8fa4a9d3
Shadan Ali <154227380+Shadan0786@users.noreply.github.com>
2025-10-29 12:25:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get('/', (req, res) => {
 });
 
 // server run karo
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
